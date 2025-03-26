89e201f02602e72dda3d85cd668e0cf22ce816e7
ByPro8 <nikoxaketi@gmail.com>
2025-03-27 03:50:09 +0400

    sd

--- src/components/MainImage.tsx
+++ src/components/MainImage.tsx
@@ -1,12 +1,8 @@
 export default function MainImage() {
   return (
-    <div className="h-screen pb-7 pt-10">
+    <div className="h-screen px-10 pb-7 pt-10">
       <div className="relative w-full max-w-full">
-        <img
-          src="/home.webp"
-          alt=""
-          className="max-h-[90%] w-full object-contain"
-        />
+        <img src="/home.webp" alt="" className="h-full w-full object-contain" />
       </div>
     </div>
   );
