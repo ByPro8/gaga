5dcfa1d0e223d28b5446a8033bf59817c06027ef
ByPro8 <nikoxaketi@gmail.com>
2025-04-09 15:36:18 +0400

    gg

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,37 +11,37 @@ const fadeVariant = {
 
 export default function Home() {
   return (
-    <motion.div
-      variants={fadeVariant}
-      initial="initial"
-      animate="animate"
-      exit="exit"
-      transition={{ duration: 0.4 }}
-      className="flex min-h-screen w-full flex-col lg:flex-row"
-    >
-      {/* ...rest of your Projects content (unchanged) */}
-      <div className="flex min-h-screen w-full sm:flex-col lg:flex-row">
-        <Menu isHome={true} />
-        <MainImage />
-
-        <TabletMenu />
-      </div>
-    </motion.div>
-
-    // <div className="relative min-h-screen w-full">
-    //   {/* Background image */}
-    //   <img
-    //     src="/home.webp"
-    //     alt="Background"
-    //     className="fixed inset-0 -z-10 h-full w-full object-cover"
-    //   />
-
-    //   {/* Content on top of background */}
-    //   <div className="z-10 flex min-h-screen w-full sm:flex-col lg:flex-row">
+    // <motion.div
+    //   variants={fadeVariant}
+    //   initial="initial"
+    //   animate="animate"
+    //   exit="exit"
+    //   transition={{ duration: 0.4 }}
+    //   className="flex min-h-screen w-full flex-col lg:flex-row"
+    // >
+    //   {/* ...rest of your Projects content (unchanged) */}
+    //   {/* <div className="flex min-h-screen w-full sm:flex-col lg:flex-row">
     //     <Menu isHome={true} />
+    //     <MainImage />
+
     //     <TabletMenu />
-    //     {/* other components */}
-    //   </div>
-    // </div>
+    //   </div> */}
+    // </motion.div>
+
+    <div className="relative min-h-screen w-full">
+      {/* Background image */}
+      <img
+        src="/home.webp"
+        alt="Background"
+        className="fixed inset-0 -z-10 h-full w-full object-cover"
+      />
+
+      {/* Content on top of background */}
+      <div className="z-10 flex min-h-screen w-full sm:flex-col lg:flex-row">
+        <Menu isHome={true} />
+        <TabletMenu />
+        {/* other components */}
+      </div>
+    </div>
   );
 }
