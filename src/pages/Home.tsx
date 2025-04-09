17b95885cf54f32e7e7b75c94dea90233c17f1c7
ByPro8 <nikoxaketi@gmail.com>
2025-04-09 16:19:26 +0400

    d

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,45 @@
-import Menu from "../components/Menu";
-import MainImage from "../components/MainImage";
-import TabletMenu from "../components/TabletMenu";
 import { motion } from "framer-motion";
+import Menu from "../components/Menu";
+import TabletMenu from "../components/TabletMenu";
 
-const fadeVariant = {
-  initial: { opacity: 4 },
-  animate: { opacity: 4 },
-  exit: { opacity: 4 },
+// Define the animation variants
+const pageVariants = {
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
 };
 
-export default function Home() {
+const pageTransition = {
+  duration: 0.5,
+  ease: "easeInOut",
+};
+
+export default function Projects() {
   return (
     <motion.div
-      variants={fadeVariant}
+      variants={pageVariants}
       initial="initial"
       animate="animate"
       exit="exit"
-      transition={{ duration: 0.4 }}
-      className="flex min-h-screen w-full flex-col lg:flex-row"
+      transition={pageTransition}
+      className=""
     >
-      <div className="flex min-h-screen w-full sm:flex-col lg:flex-row">
-        <Menu isHome={true} />
-        <MainImage />
-
-        <TabletMenu />
+      <div className="relative min-h-screen w-full">
+        {" "}
+        <img
+          src="/home.webp"
+          alt="Background"
+          className="fixed inset-0 -z-10 h-full w-full object-cover"
+        />
+        {/* Content on top of background */}
+        <div className="z-10 flex min-h-screen w-full sm:flex-col lg:flex-row">
+          <Menu isHome={true} />
+          <TabletMenu />
+          {/* other components */}
+        </div>
       </div>
     </motion.div>
-
-    // <div className="relative min-h-screen w-full">
-    //   <img
-    //     src="/home.webp"
-    //     alt="Background"
-    //     className="fixed inset-0 -z-10 h-full w-full object-cover"
-    //   />
-
-    //   {/* Content on top of background */}
-    //   <div className="z-10 flex min-h-screen w-full sm:flex-col lg:flex-row">
-    //     <Menu isHome={true} />
-    //     <TabletMenu />
-    //     {/* other components */}
-    //   </div>
-    // </div>
   );
 }
+
+//
